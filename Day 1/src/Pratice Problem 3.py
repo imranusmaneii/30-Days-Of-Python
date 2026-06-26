e474a1974fd604b231e266204976705ee8d9903c
Imran Usmani <iusmani364@gmail.com>
2026-06-26 23:57:33 +0500

    Delete Day 1/src/Pratice Problem 3.py

--- Day 1/src/Pratice Problem 3.py
+++
@@ -1,5 +0,0 @@
-print(type(100))        # Output; Integer
-print(type(100.5))      # Output: Float
-print(type("Python"))   # Output: String
-print(type(4 + 2j))     # Output: Complex
-print(type([1,2,3]))    # Output: List
