595c44a41e5ee23423336483cb01f330c88a1ce7
Imran Usmani <iusmani364@gmail.com>
2026-06-26 23:58:33 +0500

    Update Exercise1.py

--- Day 1/src/Exercise1.py
+++ Day 1/src/Exercise1.py
@@ -1,1 +1,25 @@
+# ➖➖➖➖ EXERCISE ➖➖➖➖
 
+# 1. Printing Basics
+print(15 + 7)
+print(15-7)
+print(15 * 7)
+print(15 / 7)
+print(15 // 7)
+print(15 % 7)
+print(15 ** 2)
+
+# 2. Predicting The Output
+print(20 // 6) #Output: 3
+print(20 % 6)    #Output: 2
+print(2 ** 5)  #Output: 32
+print(7 / 2)   #Output: 3.5
+
+# 3. Printing the Data Type 
+print(type(100))             # Output; Integer
+print(type(100.5))           # Output: Float
+print(type("Python"))        # Output: String
+print(type(4 + 2j))          # Output: Complex
+print(type([1,2,3]))    # Output: List
+
+# ➖➖➖➖➖ ✖️ ➖➖➖➖➖
